f21eccd28746cd3ea708b5bc02eaec4b050c6099
dawmsi <da._.off@outlook.com>
2024-03-06 12:11:51 +0200

    lets start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppLayout from "./AppLayout";
+import "../../styles/index.scss";
+
+const App = () => (
+  <div className="app">
+    <AppLayout />
+  </div>
+);
+export default App;
---
+++ src/components/App/AppLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import AppRoutes from "./AppRoutes";
+import Footer from "../Footer/Footer";
+
+const AppLayout = () => (
+  <>
+    <Header />
+    <AppRoutes />
+    <Footer />
+  </>
+);
+
+export default AppLayout;
